f349b23fa2ff3a9704aefd55e43672d76fb24223
Jeff Franklin <jefr@redhat.com>
2023-11-13 21:29:29 -0500

    Fix prod kafka and add shell script for push

---
+++ build.sh
@@ -0,0 +1,3 @@
+docker buildx build --platform=linux/amd64 -f src/main/docker/Dockerfile.jvm -t jefrankl/parks-app .
+docker tag jefrankl/parks-app quay.io/jefrankl/parks-app
+docker push quay.io/jefrankl/parks-app
