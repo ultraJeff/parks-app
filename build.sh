12e4792410eea21c1d1c88d063a85537b834edc1
Jeff Franklin <jefr@redhat.com>
2023-11-13 22:08:35 -0500

    Forgot to include mvn package in build shell script

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+mvn package -DskipTests
 docker buildx build --platform=linux/amd64 -f src/main/docker/Dockerfile.jvm -t jefrankl/parks-app .
 docker tag jefrankl/parks-app quay.io/jefrankl/parks-app
 docker push quay.io/jefrankl/parks-app
